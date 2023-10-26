974eeba1ec2f85a099d406570298debd607d9c49
luiggy <luiggylml@gmail.com>
2023-10-26 11:27:44 -0500

    fix: security check sonarlint

--- public/mockServiceWorker.js
+++ public/mockServiceWorker.js
@@ -112,7 +112,7 @@ self.addEventListener('fetch', function (event) {
   }
 
   // Generate unique request ID.
-  const requestId = Math.random().toString(16).slice(2)
+  const requestId = crypto.getRandomValues(new Uint8Array(16)).join()
 
   event.respondWith(
     handleRequest(event, requestId).catch((error) => {
