7d3f5b1f90694cc56616b97083ce3e3e59e17b76
lgcarlinf <lgcarlinf@users.noreply.github.com>
2023-10-26 16:04:06 +0000

    deploy: f51ffc9f359f46d35070b9d1de7a3d359dc27413

--- dist/assets/NotFound-589759fd.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./material-f75e056b.js";import"./react-eb403da2.js";const n=()=>o.jsx("div",{children:"NotFound"});export{n as default};
---
+++ dist/assets/NotFound-f8624d40.js
@@ -0,0 +1,1 @@
+import{j as o}from"./material-f6cb11d1.js";import"./react-eb403da2.js";const n=()=>o.jsx("div",{children:"NotFound"});export{n as default};
