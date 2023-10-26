606ddd3e7f1660f1672310c7f4a3bc93c065d966
lgcarlinf <lgcarlinf@users.noreply.github.com>
2023-10-26 06:33:46 +0000

    deploy: 3b1d5eaf90803ecfc7638e67b6e52a0a9d74cd0d

---
+++ dist/assets/Admin-308e35b7.js
@@ -0,0 +1,1 @@
+import{j as r,B as s,a}from"./material-f75e056b.js";import"./react-eb403da2.js";const o=()=>r.jsx(s,{display:"flex",gap:3,p:3,children:r.jsx(a,{children:"Admin"})});export{o as default};
---
+++ dist/assets/Login-69c1f95e.js
@@ -0,0 +1,1 @@
+import{j as o,B as r,a as s}from"./material-f75e056b.js";import"./react-eb403da2.js";const i=()=>o.jsx(r,{children:o.jsx(s,{children:"Login"})});export{i as default};
