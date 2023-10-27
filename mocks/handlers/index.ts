e1aee32588e877fc731228f413fffdc5a7145efc
Shadow <pieroramirez810@gmail.com>
2023-10-26 22:13:50 -0500

    docs: added env example

--- mocks/handlers/index.ts
+++ mocks/handlers/index.ts
@@ -1,4 +1,13 @@
+import { rest } from "msw";
 
 export const handlers = [
-
+  rest.get('/resource', (_req, res, ctx) => {
+    return res(
+      // Respond with a 200 status code
+      ctx.status(200),
+      ctx.json({
+        name: 'puerco potter'
+      })
+    )
+  })
 ]
