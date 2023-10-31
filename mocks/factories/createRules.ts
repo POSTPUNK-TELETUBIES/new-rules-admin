84193c1dfea79138b486c32fbb94b1740c9b2497
Jaen Figueroa <jaenfigueroa03042000@gmail.com>
2023-10-31 06:28:52 -0500

    docs: add RulesTable to storybook

--- mocks/factories/createRules.ts
+++ mocks/factories/createRules.ts
@@ -1,38 +1,35 @@
 import { faker, simpleFaker } from '@faker-js/faker'
 
 export const createRules = (count: number) => {
-  return {
+  return Array.from({ length: count }, () => ({
     id: simpleFaker.string.uuid(),
-    rules: Array.from({ length: count }, () => ({
-      id: simpleFaker.string.uuid(),
-      code: `${faker.helpers.arrayElement([
-        'html',
-        'css',
-        'javascript',
-        'php',
-        'python',
-        'java',
-        'ruby',
-        'go',
-        'swift',
-        'kotlin',
-      ])}:${faker.number.int({ min: 50000, max: 60000 })}`,
-      description: faker.lorem.sentence({ min: 10, max: 15 }),
-      type: faker.helpers.arrayElement([
-        'BUG',
-        'VULNERABILITY',
-        'CODE_SMELL',
-        'SECURITY_HOTSPOT',
-      ]),
-      severity: faker.helpers.arrayElement([
-        'INFO',
-        'MINOR',
-        'MAJOR',
-        'CRITICAL',
-        'BLOCKER',
-      ]),
-      state: faker.helpers.arrayElement(['ACTIVE', 'INACTIVE']),
-      date: faker.date.anytime(),
-    })),
-  }
+    code: `${faker.helpers.arrayElement([
+      'html',
+      'css',
+      'javascript',
+      'php',
+      'python',
+      'java',
+      'ruby',
+      'go',
+      'swift',
+      'kotlin',
+    ])}:${faker.number.int({ min: 50000, max: 60000 })}`,
+    description: faker.lorem.sentence({ min: 10, max: 15 }),
+    type: faker.helpers.arrayElement([
+      'BUG',
+      'VULNERABILITY',
+      'CODE_SMELL',
+      'SECURITY_HOTSPOT',
+    ]),
+    severity: faker.helpers.arrayElement([
+      'INFO',
+      'MINOR',
+      'MAJOR',
+      'CRITICAL',
+      'BLOCKER',
+    ]),
+    state: faker.helpers.arrayElement(['ACTIVE', 'INACTIVE']),
+    date: faker.date.anytime(),
+  }))
 }
---
+++ src/stories/RulesTable.stories.tsx
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { MRT_ColumnDef, MaterialReactTable } from 'material-react-table'
+import { MRT_Localization_ES } from 'material-react-table/locales/es'
+import { columns } from '../components/RulesTable/tableColumns'
+import { createRules } from '../../mocks/factories/createRules'
+
+const meta = {
+  title: 'Components/RulesTable',
+  component: MaterialReactTable,
+  tags: ['autodocs'],
+} satisfies Meta<typeof MaterialReactTable>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+  args: {
+    data: createRules(5),
+    columns: columns as MRT_ColumnDef<Record<string, unknown>>[],
+    localization: MRT_Localization_ES,
+    initialState: {
+      showGlobalFilter: true,
+      columnPinning: { right: ['mrt-row-actions'] },
+      showColumnFilters: true,
+    },
+    positionGlobalFilter: 'left',
+    muiTableHeadCellFilterTextFieldProps: {
+      sx: { m: '0.5rem 0' },
+      variant: 'outlined',
+      size: 'small',
+      placeholder: 'Todos',
+    },
+    muiSearchTextFieldProps: {
+      placeholder: `Buscar`,
+      sx: { minWidth: '18.8rem' },
+      variant: 'outlined',
+      size: 'small',
+      style: {
+        paddingTop: '0.3rem',
+        paddingLeft: '0.5rem',
+      },
+    },
+  },
+}
