f82e1afbd06fe37b5cbe365663b9f47a96d06dcc
Jaen Figueroa <jaenfigueroa03042000@gmail.com>
2023-10-31 18:55:54 -0500

    docs: add ToogleTheme to Storybook

---
+++ src/stories/ToogleTheme.stories.tsx
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import ToogleTheme from '../theme/ToogleTheme'
+import { ColorModeProvider } from '../theme/ColorModeProvider'
+import { Box, Paper, Typography } from '@mui/material'
+
+const meta = {
+  title: 'Components/ToogleTheme',
+  component: ToogleTheme,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <ColorModeProvider>
+        <Box
+          sx={{
+            px: 3,
+            py: 1,
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            width: '50rem',
+            backgroundColor: 'primary.light',
+          }}
+        >
+          <Typography fontWeight='bold' fontSize={'1.2rem'}>
+            Pacifico
+          </Typography>
+          <Story />
+        </Box>
+        <Paper
+          sx={{
+            p: 4,
+          }}
+        >
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, impedit.
+        </Paper>
+      </ColorModeProvider>
+    ),
+  ],
+} satisfies Meta<typeof ToogleTheme>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {}
