b695d4b8539785fd3242ea7829aa1e48038b4171
Jaen Figueroa <jaenfigueroa03042000@gmail.com>
2023-11-02 11:38:49 -0500

    refactor: add limit and offset

--- mocks/handlers/index.ts
+++ mocks/handlers/index.ts
@@ -2,7 +2,13 @@ import { rest } from 'msw'
 import { createRules } from './../factories/createRules'
 
 export const handlers = [
-  rest.get('/api/rules', (_req, res, ctx) => {
-    return res(ctx.status(200), ctx.json({ rules: createRules(8000) }))
+  rest.get('/api/rules', (req, res, ctx) => {
+    const limit = Number(req.url.searchParams.get('limit')) || 20
+    const offset = Number(req.url.searchParams.get('offset')) || 0
+
+    const shouldUseCustomLimit = limit && offset
+    const rules = shouldUseCustomLimit ? createRules(limit) : createRules(7000)
+
+    return res(ctx.status(200), ctx.json({ rules }))
   }),
 ]
