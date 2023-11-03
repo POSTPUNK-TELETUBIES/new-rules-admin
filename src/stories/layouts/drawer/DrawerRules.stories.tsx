7d7de689d64d58802e5a1d3bcf3439b092825077
practice <eloat.debedu@gmail.com>
2023-11-03 10:55:20 -0500

    feat: drawer storybook

---
+++ src/stories/layouts/drawer/DrawerRules.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import DrawerRules from '../../../components/layouts/DrawerRules';
+
+const meta = {
+  title: 'Layouts/Drawer Rules',
+  component: DrawerRules,
+  parameters: {
+    layout: 'centered'
+  },
+
+} satisfies Meta<typeof DrawerRules>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Default: Story = {
+  args: {
+  }
+}
