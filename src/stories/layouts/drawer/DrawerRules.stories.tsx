5c098f8b39650fb8540efced1d44aee308496e9f
Jaen Figueroa <jaenfigueroa03042000@gmail.com>
2023-11-08 07:09:38 -0500

    docs: update Drawer component

--- src/stories/layouts/drawer/DrawerRules.stories.tsx
+++ src/stories/layouts/drawer/DrawerRules.stories.tsx
@@ -1,20 +1,50 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import DrawerRules from '../../../components/layouts/DrawerRules';
+import type { Meta, StoryObj } from '@storybook/react'
+import DrawerRules from '../../../components/layouts/DrawerRules'
+import { ColorModeProvider } from '../../../theme/ColorModeProvider'
+import {
+  DrawerContext,
+  DrawerProvider,
+} from '../../../components/layouts/Drawercontext'
+import { useContext } from 'react'
+import { Button } from '@mui/material'
 
 const meta = {
   title: 'Layouts/Drawer Rules',
   component: DrawerRules,
   parameters: {
-    layout: 'centered'
+    layout: 'centered',
   },
+  decorators: [
+    (Story) => {
+      return (
+        <ColorModeProvider>
+          <DrawerProvider>
+            <DrawerContainer>
+              <Story />
+            </DrawerContainer>
+          </DrawerProvider>
+        </ColorModeProvider>
+      )
+    },
+  ],
+} satisfies Meta<typeof DrawerRules>
 
-} satisfies Meta<typeof DrawerRules>;
-
-export default meta;
-type Story = StoryObj<typeof meta>;
+export default meta
+type Story = StoryObj<typeof meta>
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Default: Story = {
-  args: {
-  }
+  args: {},
+}
+
+const DrawerContainer = ({ children }: { children: JSX.Element }) => {
+  const { setIsOpenDrawer } = useContext(DrawerContext)
+  return (
+    <>
+      <Button variant='outlined' onClick={() => setIsOpenDrawer(true)}>
+        Abrir drawer
+      </Button>
+      {children}
+    </>
+  )
 }
