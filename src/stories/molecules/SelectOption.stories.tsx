07ef1ec1a767bb8518eeeee0858294304de7ce07
Shadow <pieroramirez810@gmail.com>
2023-11-13 11:09:36 -0500

    fix: solved conflicts

---
+++ src/stories/molecules/SelectOption.stories.tsx
@@ -0,0 +1,51 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import SelectOptions from '../../components/molecules/SelectOptions'
+import PersonIcon from '@mui/icons-material/Person'
+import LogoutIcon from '@mui/icons-material/Logout'
+import SettingsIcon from '@mui/icons-material/Settings'
+import HelpIcon from '@mui/icons-material/Help'
+import ChatIcon from '@mui/icons-material/Chat'
+
+const meta = {
+  title: 'molecules/SelectOptions',
+  component: SelectOptions,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    icon: { control: 'text' },
+    items: { control: 'array' },
+  },
+} satisfies Meta<typeof SelectOptions>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+  args: {
+    icon: <PersonIcon />,
+    items: [
+      {
+        icon: <HelpIcon />,
+        text: 'Ayuda y soporte técnico',
+        onClick: () => {},
+      },
+      {
+        icon: <SettingsIcon />,
+        text: 'Configuración y privacidad',
+        onClick: () => {},
+      },
+      {
+        icon: <ChatIcon />,
+        text: 'Enviar comentarios',
+        onClick: () => {},
+      },
+      {
+        icon: <LogoutIcon />,
+        text: 'Cerrar sesión',
+        onClick: () => {},
+      },
+    ],
+  },
+}
