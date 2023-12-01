b776842e09d984466898d059cb96f4a792d69bd3
Jaen Figueroa <jaenfigueroa03042000@gmail.com>
2023-12-01 07:22:53 -0500

    refactor: remove duplicate worker.start

--- src/AppProviders.tsx
+++ src/AppProviders.tsx
@@ -1,17 +1,8 @@
 import { PropsWithChildren } from 'react'
 import { ColorModeProvider } from './theme/ColorModeProvider.tsx'
-import { isMSWOn } from './config/worker.ts'
 import { DataProvider } from 'data_providers'
 import { providerNames } from './dataProviders/index.ts'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
-if (import.meta.env.DEV && isMSWOn) {
-  const { worker } = await import('../mocks/browser.ts')
-
-  await worker.start({
-    waitUntilReady: true,
-  })
-}
 
 const queryClient = new QueryClient()
 
