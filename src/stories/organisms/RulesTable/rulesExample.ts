99ce04c2d604f062433c477dbf132e4acb6128e1
Jaen Figueroa <jaenfigueroa03042000@gmail.com>
2024-01-05 11:17:19 -0500

    fix: resolve small conflicts

--- src/stories/organisms/RulesTable/rulesExample.ts
+++ src/stories/organisms/RulesTable/rulesExample.ts
@@ -2,6 +2,6 @@ import { createOneRule } from '../../../../mocks/factories/createOneRule'
 import { RuleDTO } from '../../../types/rule'
 
 export const rulesExample: RuleDTO[] = Array.from({ length: 100 }, () => {
-  const rule: RuleDTO = createOneRule()
+  const rule: RuleDTO = { ...createOneRule(), history: [] }
   return rule
 })
