32617e24716e3698c56702d7bcdc3c535e4241ef
ChumanY <cuerpoespin_2000@hotmail.com>
2024-01-08 12:40:17 -0500

    fix: sonarcloud errors

--- src/components/ExcelReport/dowloandExcelFile.tsx
+++ src/components/ExcelReport/dowloandExcelFile.tsx
@@ -1,30 +1,44 @@
 import { CombinedDataDTO } from './combinedDB'
 import * as XLSX from 'xlsx'
 
-function downloadExcelFile(combinedData: CombinedDataDTO[]): void {
-  const workbook = XLSX.utils.book_new()
+function downloadExcelFile(combinedData: CombinedDataDTO[]): Promise<void> {
+  return new Promise((resolve, reject) => {
+    try {
+      const workbook = XLSX.utils.book_new()
+      const worksheet = XLSX.utils.json_to_sheet(combinedData)
+      XLSX.utils.book_append_sheet(
+        workbook,
+        worksheet,
+        'SonarQube_Rules_Status',
+      )
+      const excelData = XLSX.write(workbook, {
+        bookType: 'xlsx',
+        type: 'array',
+      })
+      const blob = new Blob([excelData], {
+        type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      })
 
-  const worksheet = XLSX.utils.json_to_sheet(combinedData)
+      const url = URL.createObjectURL(blob)
 
-  XLSX.utils.book_append_sheet(workbook, worksheet, 'SonarQube_Rules_Status')
+      const a = document.createElement('a')
+      a.href = url
+      a.download = 'SonarQube_Rules_Status.xlsx'
 
-  const excelData = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' })
+      document.body.appendChild(a)
 
-  const blob = new Blob([excelData], {
-    type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+      a.addEventListener('load', () => {
+        document.body.removeChild(a)
+        URL.revokeObjectURL(url)
+        resolve()
+      })
+
+      a.click()
+    } catch (error) {
+      console.error('Error al descargar Excel:', error)
+      reject(error)
+    }
   })
-
-  const url = URL.createObjectURL(blob)
-
-  const a = document.createElement('a')
-  a.href = url
-  a.download = 'SonarQube_Rules_Status.xlsx'
-
-  document.body.appendChild(a)
-  a.click()
-
-  document.body.removeChild(a)
-  URL.revokeObjectURL(url)
 }
 
 export default downloadExcelFile
