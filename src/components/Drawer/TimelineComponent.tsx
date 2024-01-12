dbf3c6c09dd1c7ea9084d9e932dbcc6789c6c518
ChumanY <cuerpoespin_2000@hotmail.com>
2024-01-12 07:54:04 -0500

    feat: add paging to the rule history

--- src/components/Drawer/TimelineComponent.tsx
+++ src/components/Drawer/TimelineComponent.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react'
+import React, { Suspense, useState } from 'react'
 import { Button, LinearProgress } from '@mui/material'
 import Timeline from '@mui/lab/Timeline'
 import TimelineItem from '@mui/lab/TimelineItem'
@@ -25,48 +25,76 @@ export const TimelineSection: React.FC<TimelineSectionProps> = ({
   order,
   handleChangeOrder,
 }) => {
+  const itemsPerPage = 20
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const indexOfLastItem = currentPage * itemsPerPage
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage
+  const currentItems = sortedHistory.slice(indexOfFirstItem, indexOfLastItem)
+
+  const totalPages = Math.ceil(sortedHistory.length / itemsPerPage)
+
+  const handleNextPage = () => {
+    setCurrentPage((prevPage) => Math.min(prevPage + 1, totalPages))
+  }
+
+  const handlePrevPage = () => {
+    setCurrentPage((prevPage) => Math.max(prevPage - 1, 1))
+  }
+
   return (
-    <Timeline
-      sx={{
-        [`& .${timelineOppositeContentClasses.root}`]: {
-          flex: 0.2,
-        },
-      }}
-    >
-      <Button onClick={handleChangeOrder}>
-        {order === Order.Ascending
-          ? 'Ordenar de manera descendente'
-          : 'Ordenar de manera ascendente'}
-      </Button>
-      {sortedHistory?.map((item, index) => (
-        <TimelineItem key={item.id}>
-          <TimelineOppositeContent>
-            <Suspense fallback={<LinearProgress />}>
-              {getDateDifference(item.time)}
-            </Suspense>
-          </TimelineOppositeContent>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>
-            <Suspense fallback={<LinearProgress />}>
-              <CardHistory
-                id={item.id}
-                avatar={item.avatar}
-                user={item.user}
-                action={item.action}
-                sustain={item.sustain}
-                showEditButton={
-                  order === Order.Ascending
-                    ? index === sortedHistory.length - 1
-                    : index === 0
-                }
-              />
-            </Suspense>
-          </TimelineContent>
-        </TimelineItem>
-      ))}
-    </Timeline>
+    <>
+      <Timeline
+        sx={{
+          [`& .${timelineOppositeContentClasses.root}`]: {
+            flex: 0.2,
+          },
+        }}
+      >
+        <Button onClick={handleChangeOrder}>
+          {order === Order.Ascending
+            ? 'Ordenar de manera descendente'
+            : 'Ordenar de manera ascendente'}
+        </Button>
+        {currentItems?.map((item, index) => (
+          <TimelineItem key={item.id}>
+            <TimelineOppositeContent>
+              <Suspense fallback={<LinearProgress />}>
+                {getDateDifference(item.time)}
+              </Suspense>
+            </TimelineOppositeContent>
+            <TimelineSeparator>
+              <TimelineDot />
+              <TimelineConnector />
+            </TimelineSeparator>
+            <TimelineContent>
+              <Suspense fallback={<LinearProgress />}>
+                <CardHistory
+                  id={item.id}
+                  avatar={item.avatar}
+                  user={item.user}
+                  action={item.action}
+                  sustain={item.sustain}
+                  showEditButton={
+                    order === Order.Ascending
+                      ? index === sortedHistory.length - 1
+                      : index === 0
+                  }
+                />
+              </Suspense>
+            </TimelineContent>
+          </TimelineItem>
+        ))}
+      </Timeline>
+
+      <div>
+        <Button onClick={handlePrevPage} disabled={currentPage === 1}>
+          Página anterior
+        </Button>
+        <Button onClick={handleNextPage} disabled={currentPage === totalPages}>
+          Página siguiente
+        </Button>
+      </div>
+    </>
   )
 }
