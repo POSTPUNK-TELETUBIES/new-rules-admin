42297f4e09e84a47557caa10b44f5566e7968aa0
luiggy <luiggylml@gmail.com>
2024-01-24 11:57:28 -0500

    fix: remove unused file for deployment fix

--- src/types/mswTypes.ts
+++
@@ -1,9 +0,0 @@
-import {
-  DefaultBodyType,
-  PathParams,
-  ResponseComposition,
-  RestRequest,
-} from 'msw'
-
-export type RequestTypeMSW = RestRequest<never, PathParams<string>>
-export type ResponseTypeMSW = ResponseComposition<DefaultBodyType>
